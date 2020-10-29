ec38caf2ca45be58d89af9b0dae216c89de45b73
parahatreis <parahat.iljanow@gmail.com>
2020-10-29 19:23:01 +0500

    front-end

---
+++ build/precache-manifest.9cf7974ef3de2ab6b67b0a271ef77553.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e9105f9d54bcc9b29fd05c64bbce32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed49a7ffe166e004f3a7",
+    "url": "/static/css/main.86d11353.chunk.css"
+  },
+  {
+    "revision": "596c348c5cec9c939d70",
+    "url": "/static/js/2.91cb25be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91cb25be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed49a7ffe166e004f3a7",
+    "url": "/static/js/main.c829c2f4.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "fb67c607c8cf801ca268bc738103ba7e",
+    "url": "/static/media/Saira-Regular.fb67c607.ttf"
+  },
+  {
+    "revision": "3082ea12cd87b403e928e63c59c615aa",
+    "url": "/static/media/doc.3082ea12.png"
+  },
+  {
+    "revision": "1d0014c9be03679cb79897c8cf2df772",
+    "url": "/static/media/ppt.1d0014c9.png"
+  }
+]);
